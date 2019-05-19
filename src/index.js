8842265371d077f3a3692e968b22f7bbcb349f68
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-18 22:10:04 -0500

    added requirement Provider to index.js, have a type Error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();

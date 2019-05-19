9addbcb39620300916261e12433fd5a9b62172db
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-19 08:59:28 -0500

    typeError was fixed for Provider and Store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import storeInstance from './modules/store/store';
+import storeInstance from './modules/store/store'
 
 // Redux
 import { Provider } from 'react-redux';
@@ -11,4 +11,5 @@ import { Provider } from 'react-redux';
 
 // Wrap our App in a Provider, this makes Redux available in
 // our entire application
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
+
